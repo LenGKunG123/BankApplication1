d672bbc5242a0a567fc3b86f00bb50be3d7049ad
MarKKunG123 <lengtamma1310@gmail.com>
2023-05-10 10:30:49 +0700

    New Bank

---
+++ Account.java
@@ -0,0 +1,35 @@
+package BankApplication1;
+
+public class Account {
+    private int number;
+    private String name;
+    private double balance;
+
+    public Account (int number,String name,double balance){
+        this.name = name;
+        this.number = number;
+        this.balance = balance;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+    public void deposit (double amount) {
+        balance = balance + amount;
+        System.out.println("Deposit = " + amount);
+        System.out.println("Total balance =" + balance );
+    }
+    public void withdraw (double amount){
+        balance = balance - amount;
+        System.out.println("Withdraw = " + amount);
+        System.out.println("Total balance =" + balance );
+    }
+}
---
+++ Bank.java
@@ -0,0 +1,14 @@
+package BankApplication1;
+
+public class Bank {
+    private String name;
+
+    public Bank (){
+    }
+    public Bank(String name){
+        this.name = name;
+    }
+    public void openAccount (Account account) {
+
+    }
+}
